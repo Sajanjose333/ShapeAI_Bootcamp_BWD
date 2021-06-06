e8a65c881c28c2d4562837435c46f02b6c922720
Sajanjose333 <85438988+Sajanjose333@users.noreply.github.com>
2021-06-06 16:52:44 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+    <p>
+      I have many favorite songs.Mainly i like melody songs.Mostly malaylam melody songs.Old malayalam songs are also glorious.
+  </p>
+  </div>
+  );
+}
+export default Note;
